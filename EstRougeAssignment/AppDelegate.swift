d9d6457d058e062ac94cb6b181dc743fa1725dec
cukingpro <mr.cukingpro@gmail.com>
2020-05-31 23:01:49 +0700

    initial setup

---
+++ EstRougeAssignment/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  EstRougeAssignment
+//
+//  Created by Huy Tong on 5/31/20.
+//  Copyright © 2020 Huy Tong. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupRootViewController()
+        return true
+    }
+
+
+
+}
+
+extension AppDelegate {
+
+    private func setupRootViewController() {
+        let viewController = UIViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
+
